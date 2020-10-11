5e5bbbdbb4ca46e0dc24c2dc80ed0bf332d75205
mengda1027 <mengda1027@qq.com>
2020-10-11 21:16:22 +0800

    implement  ''createElement" func

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-let a = <div></div>
+function createElement(tagName, attributes, ...children) {
+  let e = document.createElement(tagName)
+  for (const attr in attributes) {
+    e.setAttribute(attr, attributes[attr])
+  }
+  for (const child of children) {
+    if (typeof child === "string") {
+      child = document.createTextNode(child)
+    }
+    e.appendChild(child)
+  }
+  return e
+}
+
+window.a = (
+  <div id="a" class="c">
+    <div>abc</div>
+    <div></div>
+    <div></div>
+  </div>
+)
