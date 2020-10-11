57d661c43d258f5c0f84e2ed0c18a644cafacc29
mengda1027 <mengda1027@qq.com>
2020-10-11 18:21:38 +0800

    add babel basic config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ module.exports = {
   entry: {
     main: "./main.js",
   },
+  module: {
+    rules: [{ test: /\.js$/, use: { loader: "babel-loader", options: { presets: ["@babel/preset-env"] } } }],
+  },
   mode: "development",
   optimization: {
     minimize: false,
