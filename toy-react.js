775066550d3ae13cf4fa10de33b9b05fb2c8428c
mengda1027 <mengda1027@qq.com>
2020-10-12 14:38:26 +0800

    修改为基于 Range API 的 render 方式

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,6 @@
+// 模拟私有方法
+const RENDER_TO_DOM = Symbol("render_to_dom")
+
 // 封装 document.createElement
 class ElementWrapper {
   // 构建实体dom
@@ -8,13 +11,25 @@ class ElementWrapper {
     this.root.setAttribute(k, v)
   }
   appendChild(component) {
-    this.root.appendChild(component.root)
+    let range = document.createRange()
+    // appendChild 是将插入内容放在所有节点的最后
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    component[RENDER_TO_DOM](range)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
@@ -31,15 +46,14 @@ export class Component {
   appendChild(component) {
     this.children.push(component)
   }
-  get root() {
-    if (!this._root) {
-      // 如果 render 仍返回了 Component 类型，则递归直至 ElementWrapper 或 TextWrapper
-      this._root = this.render().root
-    }
-    return this._root
+  [RENDER_TO_DOM](range) {
+    this.render()[RENDER_TO_DOM](range)
   }
 }
 
+// plugin-transform-react-jsx 将 jsx 解析后调用 createEelment
+// JSX转换机制：小写时type为字符串，包含大写时(自定义组件)type为组件自定义对象或class
+// 第三个参数起为子节点  或者  如果当前节点的子节点为文本节点则第三个参数是字符串（文本内容）
 export function createElement(type, attributes, ...children) {
   let e
   if (typeof type === "string") {
@@ -58,9 +72,10 @@ export function createElement(type, attributes, ...children) {
         child = new TextWrapper(child)
       }
       if (Array.isArray(child)) {
+        // 递归
         insertChildren(child)
       } else {
-        // 只有 TextWrapper 或 ElementWrapper 才
+        // 只有 TextWrapper 或 ElementWrapper 才添加
         e.appendChild(child)
       }
     }
@@ -71,5 +86,10 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root)
+  // 将挂载点的dom内容清空
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
